fbc4ec0f8d576955ad446a8ed2de1a98aa4d0bd7
YUENIIMAA <yueniimaa@outlook.com>
2020-06-21 10:41:21 +0800

    implement SSSP

--- sourcecode/src/main/scala/SSSP.scala
+++ sourcecode/src/main/scala/SSSP.scala
@@ -20,6 +20,42 @@ object SSSP {
     println("[info]<-------Vertex Information----->")
     iptGraph.vertices.collect.foreach(println(_))
 
+    /* specify the ultimate source */
+    val sourceId: VertexId = 0
+
+    /* set distance to 0 if the vertex is the source and INF otherwise */
+    val graph = iptGraph.mapVertices((id, _) => {
+      if (id == sourceId) 0
+      else Int.MaxValue
+    })
+
+    /* initialMsg: set every vertex's value to INF */
+    val sssp = graph.pregel(Int.MaxValue)(
+      /* vprog: receive INF from initialMsg and use min to skip the source */
+      (id, dist, newDist) => math.min(dist, newDist),
+      /* sendMsg: if src->self->dst is shorter, send the shorter distance to dst */
+      triplet => {
+        /* note that the iteration doesn't necessarily start from the source */
+        /* thus we have to take care of overflow */
+        if (triplet.srcAttr == Int.MaxValue) {
+          Iterator.empty
+        }
+        /* case mentioned above */
+        else if (triplet.srcAttr + triplet.attr < triplet.dstAttr) {
+          Iterator((triplet.dstId, triplet.srcAttr + triplet.attr))
+        }
+        /* if src->self->dst is larger, ignore */
+        else {
+          Iterator.empty
+        }
+      },
+      /* mergeMsg: if there are multiple path shorter than the current one, pick the shortest */
+      (a,b) => math.min(a, b)
+    )
+
+    println("[info]<---------Shortest Path-------->")
+    sssp.vertices.collect.foreach(println(_))
+
     spark.stop()
   }
 }
